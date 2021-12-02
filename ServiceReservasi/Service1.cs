59e4fc30955e8e32ca296000db2967396542d14b
sekarputrii <sekarputriretno83@gmail.com>
2021-12-02 10:53:30 +0700

    Add project files.

---
+++ ServiceReservasi/Service1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        string constring = "Data Source = LAPTOP-214BN04U; Initial Catalog =WCFTest; Persist Security Info=True; User ID=sa; Password=Salr8301";
+        SqlConnection connection;
+        SqlCommand com; //untuk mengkoneksikan database ke visual studio
+
+        public string deletePemesanan(string IDPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DetailLokasi> DetailLokasi()
+        {
+            List<DetailLokasi> LokasiFull = new List<DetailLokasi>();   //proses untuk mendeclare nama list yang telah dibuat dengan nama baru
+            try
+            {
+                string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.lokasi";    //declare query
+                connection = new SqlConnection(constring);  //fungsi connect ke database
+                com = new SqlCommand(sql, connection);  //proses exequte query
+                connection.Open();  //membuka koneksi
+                SqlDataReader reader = com.ExecuteReader(); //menampilkan data query
+                while (reader.Read())
+                {
+                    //nama class
+                    DetailLokasi data = new DetailLokasi(); //deklarasi data, mengambil 1persatu dari database
+                    //bentuk array
+                    data.IDLokasi = reader.GetString(0);    //0 itu index, ada di kolom keberapa di string sql diatas
+                    data.NamaLokasi = reader.GetString(1);
+                    data.DeskripsiFull = reader.GetString(2);
+                    data.Kuota = reader.GetInt32(3);
+                    LokasiFull.Add(data);   //mengumpulkan data yang awalnya dari array
+                }
+                connection.Close(); //untuk menutup akses ke database
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return LokasiFull;
+        }
+
+        public string editPemesanan(string IDPemesanan, string NamaCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Pemesanan values ('" + IDPemesanan + "','" + NamaCustomer + "','" + NoTelpon + "'," + JumlahPemesanan + ",'" + IDLokasi + "')";
+                connection = new SqlConnection(constring);  //fungsi connect ke database
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+                a = "Sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+            return a;
+        }
+
+        public List<Pemesanan> Pemesanan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CekLokasi> ReviewLokasi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
