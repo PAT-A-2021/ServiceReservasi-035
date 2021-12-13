0e7d5d0bdefa310ccf8375905956eb2e59712ba1
sekarputrii <sekarputriretno83@gmail.com>
2021-12-13 08:48:10 +0700

    P9

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -64,4 +64,26 @@ namespace ServiceReservasi
         [DataMember]
         public string Lokasi { get; set; }
     }
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
 }
--- ServiceReservasi/Service1.cs
+++ ServiceReservasi/Service1.cs
@@ -87,6 +87,22 @@ namespace ServiceReservasi
             }
             return a;
         }
+        public string GetData(int value)
+        {
+            return string.Format("You entered : {0}", value);
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
 
         public string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
         {
